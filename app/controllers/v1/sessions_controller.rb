d9bf008f172954785ea17781adff6c5d97be7ed5
Lawrence Secrease <lawrence@bookshout.com>
2017-11-13 22:52:27 -0600

    implement jbuilder

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,10 +1,11 @@
 class V1::SessionsController < ApplicationController
  
  def create
-  user = User.where(email: params[:email]).first
+  @user = User.where(email: params[:email]).first
   
-  if user&.valid_password?(params[:password])
-   render json: user.as_json(only: [:email, :authentication_token]), status: :created
+  if @user&.valid_password?(params[:password])
+   render :create, status: :created
+#   render json: user.as_json(only: [:email, :authentication_token]), status: :created
   else
    head(:unauthorized)
   end
@@ -12,7 +13,12 @@ class V1::SessionsController < ApplicationController
  
  
  def destroy
-  
+  current_user&.authenticaton_token = nil
+  if current_user.save 
+   head(:ok)
+  else
+   head(:unauthorized)
+  end
  end
  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
  devise_for :users
- namespace :v1 do
+ namespace :v1, defaults: { format: :json } do
   resources :contacts
-  resources :sessions, only: [:create, :destroy]
+  resource :sessions, only: [:create, :destroy]
  end
  
 end
